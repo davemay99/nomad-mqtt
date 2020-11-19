b61c7b36bbab8655f99292a868c4dc0cbb043e79
davemay99 <dmay@hashicorp.com>
2020-11-19 10:53:01 -0500

    add mqtt hello world publish

--- webhook_server.go
+++ webhook_server.go
@@ -6,9 +6,34 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
 func main() {
+	mqtt.ERROR = log.New(os.Stdout, "[ERROR] ", 0)
+	mqtt.CRITICAL = log.New(os.Stdout, "[CRIT] ", 0)
+	mqtt.WARN = log.New(os.Stdout, "[WARN]  ", 0)
+	mqtt.DEBUG = log.New(os.Stdout, "[DEBUG] ", 0)
+
+	connOpts := mqtt.NewClientOptions().AddBroker("tcp://192.168.1.64:1883").SetClientID("gotrivial")
+	connOpts.SetKeepAlive(2 * time.Second)
+	connOpts.SetDefaultPublishHandler(f)
+	connOpts.SetPingTimeout(1 * time.Second)
+
+	client := mqtt.NewClient(connOpts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	client.Publish("go-mqtt/sample", 0, false, "hello world")
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Args[1]), nil))
 }
